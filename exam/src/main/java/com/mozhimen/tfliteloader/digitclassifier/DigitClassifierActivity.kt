d95197e05282c21224a022b5c870e0a23f4addff
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2023-05-09 15:37:26 +0800

    0509

---
+++ exam/src/main/java/com/mozhimen/tfliteloader/digitclassifier/DigitClassifierActivity.kt
@@ -0,0 +1,12 @@
+package com.mozhimen.tfliteloader.digitclassifier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mozhimen.tfliteloader.R
+
+class DigitClassifierActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_digit_classifier)
+    }
+}
